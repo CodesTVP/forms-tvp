d7834ad6326938b322093b6e2d17d650ffa17d1d
CodesTVP <techdragonarray@gmail.com>
2023-08-16 09:17:18 -0300

    update

--- src/components/admin/ListForms.jsx
+++ src/components/admin/ListForms.jsx
@@ -16,7 +16,6 @@ import {
 } from '@mui/material'
 import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore'
 import { useContext } from 'react'
-import { homepage } from '../../../package.json'
 import { FormContext } from '../../contexts/FormContext'
 import { FormVisibleContext } from '../../contexts/FormVisibleContext'
 import { AllFormsContext } from '../../contexts/ListFormsContext'
@@ -30,7 +29,7 @@ function ListForms(props) {
     const embed = (form) => {
         const formId = form.id
         const stringIframe = `<iframe width="100%" height="550px" frameborder="none" src="${
-            homepage + '#/form/' + formId
+            'https://codestvp.github.io/forms-tvp//#/form/' + formId
         }" id="${formId}"></iframe>`
         const stringScript = `<script>function receive(event){const brokenData=event.data?.toString()?.split(' ');if(brokenData.length===2){const [action,value]=[brokenData[0],brokenData[1]];if(action==='set-iframe-form-height'){document.getElementById('${formId}').height=(parseInt(value)+60)+'px';}}};window.addEventListener("message",receive);</script>`
         const code = stringIframe + stringScript
